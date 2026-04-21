7259e6950dad0935669b281cbd2e0afc0256511c
Crasher <wiktorszaf83@gmail.com>
2026-04-21 12:46:47 +0200

    Innitial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 				<ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
 					<li className="mb-2 tracking-[-.01em]">
 						Get started by editing{" "}
-						<code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+						<code className="bg-black/5 dark:bg-white/6 font-mono font-semibold px-1 py-0.5 rounded">
 							src/app/page.tsx
 						</code>
 						.
@@ -18,7 +18,7 @@ export default function Home() {
 
 				<div className="flex gap-4 items-center flex-col sm:flex-row">
 					<a
-						className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+						className="rounded-full border border-solid border-black/8 dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#6e5151] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
 						href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
 						target="_blank"
 						rel="noopener noreferrer"
