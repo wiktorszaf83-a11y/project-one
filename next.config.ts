0e087fd3ca331105dacc352772d64c8d3d050a41
wiktorszaf83-a11y <wiktorszaf83@gmail.com>
2026-05-03 21:43:36 +0200

    Clean up next.config.ts by removing placeholder comment
    Removed placeholder comment for future Next.js settings.

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  // Tutaj znajdą się Twoje ewentualne przyszłe ustawienia Next.js
 };
 
 export default withNextIntl(nextConfig);
